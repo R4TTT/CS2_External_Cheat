d90ded8a84f0b67893fa3a5f501d9abca5f3f10f
R4TTT <atakanyasar007@gmail.com>
2023-10-25 20:53:54 +0300

    CS2_External_Cheat

---
+++ Offsets.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <Windows.h>
+#include "Utils/ProcessManager.hpp"
+
+// https://www.unknowncheats.me/forum/counter-strike-2-a/576077-counter-strike-2-reversal-structs-offsets-89.html
+namespace Offset
+{
+	inline DWORD EntityList;
+	inline DWORD Matrix;
+	inline DWORD ViewAngle;
+	inline DWORD LocalPlayerController;
+	inline DWORD LocalPlayerPawn;
+	inline DWORD ForceJump;
+	inline DWORD GlobalVars;
+
+	struct
+	{
+		DWORD Health = 0x32C;
+		DWORD TeamID = 0x3BF;
+		DWORD IsAlive = 0x7C4;
+		DWORD PlayerPawn = 0x7BC;
+		DWORD iszPlayerName = 0x610;
+	}Entity;
+
+	struct
+	{
+		DWORD Pos = 0x1214;
+		DWORD MaxHealth = 0x328;
+		DWORD CurrentHealth = 0x32C;
+		DWORD GameSceneNode = 0x310;
+		DWORD BoneArray = 0x1E0;
+		DWORD angEyeAngles = 0x14F8;
+		DWORD vecLastClipCameraPos = 0x1274;
+		DWORD pClippingWeapon = 0x1290;
+		DWORD iShotsFired = 0x1400;
+		DWORD flFlashDuration = 0x1450;
+		DWORD aimPunchAngle = 0x16FC;
+		DWORD aimPunchCache = 0x1720;
+		DWORD iIDEntIndex = 0x1524;
+		DWORD iTeamNum = 0x3BF;
+		DWORD CameraServices = 0x10E0;
+		DWORD iFovStart = 0x214;
+		DWORD fFlags = 0x3C8;
+		DWORD bSpottedByMask = 0x1618 + 0xC; // entitySpottedState + bSpottedByMask
+	}Pawn;
+
+	struct
+	{
+		DWORD RealTime = 0x00;
+		DWORD FrameCount = 0x04;
+		DWORD MaxClients = 0x10;
+		DWORD IntervalPerTick = 0x14;
+		DWORD CurrentTime = 0x2C;
+		DWORD CurrentTime2 = 0x30;
+		DWORD TickCount = 0x40;
+		DWORD IntervalPerTick2 = 0x44;
+		DWORD CurrentNetchan = 0x0048;
+		DWORD CurrentMap = 0x0180;
+		DWORD CurrentMapName = 0x0188;
+	} GlobalVar;
+
+	namespace Signatures
+	{
+		const std::string GlobalVars = "48 89 0D ?? ?? ?? ?? 48 89 41";
+		const std::string ViewMatrix = "48 8D 0D ?? ?? ?? ?? 48 C1 E0 06";
+		const std::string ViewAngles = "48 8B 0D ?? ?? ?? ?? 48 8B 01 48 FF 60 30";
+		const std::string EntityList = "48 8B 0D ?? ?? ?? ?? 48 89 7C 24 ?? 8B FA C1";
+		const std::string LocalPlayerController = "48 8B 05 ?? ?? ?? ?? 48 85 C0 74 4F";
+		const std::string ForceJump = "48 8B 05 ?? ?? ?? ?? 48 8D 1D ?? ?? ?? ?? 48 89 45";
+		const std::string LocalPlayerPawn = "48 8D 05 ?? ?? ?? ?? C3 CC CC CC CC CC CC CC CC 48 83 EC ?? 8B 0D";
+	}
+
+	bool UpdateOffsets();
+}
